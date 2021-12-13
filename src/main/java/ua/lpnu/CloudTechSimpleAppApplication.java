909d95a83af57f5c805619a3310f9447bfa873a1
Julia <juliakurylo18@gmail.com>
2021-12-13 22:53:56 +0200

    initial commit

---
+++ src/main/java/ua/lpnu/CloudTechSimpleAppApplication.java
@@ -0,0 +1,13 @@
+package ua.lpnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTechSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTechSimpleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lpnu/CloudTechSimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lpnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTechSimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
